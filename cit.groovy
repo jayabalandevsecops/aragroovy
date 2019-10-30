f9de6cc9795999d78bb01ea234286a0fb5d2740b
jayabalandevsecops <jayabalandevops@gmail.com>
2019-10-31 00:05:50 +0530

    first commit

--- cit.groovy
+++ cit.groovy
@@ -1,17 +1,22 @@
 node("$NodeName"){
-wrks = env.WORKSPACE
-stage("Prepare"){
-    println("Preparing the environment...")
-    git(
-        url: "git@github.com:jayabalandevsecops/aragroovy.git",
-        branch: "master"
-    )
-    dir('Config'){
+    wrks = env.WORKSPACE
+    stage("Prepare"){
+        println("Preparing the environment...")
         git(
-            url: "git@github.com:jayabalandevsecops/aragroovyconf.git",
+            url: "git@github.com:jayabalandevsecops/aragroovy.git",
             branch: "master"
         )
+        dir('Config'){
+            git(
+                url: "git@github.com:jayabalandevsecops/aragroovyconf.git",
+                branch: "master"
+            )
+        }
+        println("Prepared the environment..!")
     }
-    println("Prepared the environment..!")
-}
+    stage("Clone"){
+        println("Cloning the App repo...")
+        load('app/clone.groovy')
+        println("Cloned the app repo..!")
+    }
 }

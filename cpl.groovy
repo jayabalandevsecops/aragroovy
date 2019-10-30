5fb4b65c65bc5fa8a468ca36edb14cf6aa5ed0f1
jayabalandevsecops <jayabalandevops@gmail.com>
2019-10-30 23:11:52 +0530

    added parameters to cpl . groovy

--- cpl.groovy
+++ cpl.groovy
@@ -1,5 +1,26 @@
 folder("$BUName"){}
 folder("$BUName/$ProductName"){}
 pipelineJob("$BUName/$ProductName/CICD_$AppName"){
-
+    parameters{
+        stringParam("AppRepo", "$ApplicationRepo", "GIT URL - App Repo")
+        stringParam("AppName", "$AppName", "App Nane")
+        stringParam("TestRunTool", "$UniTestRun", "")
+        stringParam("NodeName", "$NodeName", "")
+        activeChoiceParam('Branch'){
+            description('Branch to be build')
+            choiceType('SINGLE_SELECT')
+            groovyScript{
+                script('["Select", "Develop", "QA", "Test"]')
+                fallbackScript('Fallback choice')
+            }
+        }
+    }
+    definition{
+        cps{
+            def jobScript = new readFileFromWorkspace('cit.groovy')
+            script(jobScript)
+            def approvals = org.jenkinsci.plugins.sciptsecurity.scripts.ScriptApprovals.get()
+            approvals.approveScript(approvals.hash(jobScript, "groovy"))
+        }
+    }
 }

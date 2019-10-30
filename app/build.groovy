21fe4506faa8c00673d7cb362f7dc5b393ce7178
jayabalandevsecops <jayabalandevops@gmail.com>
2019-10-31 00:28:09 +0530

    stage build added

--- app/build.groovy
+++ app/build.groovy
@@ -4,4 +4,4 @@ cDelivery = readFile "$wrks/config/cpl.json"
 def jString = new JsonSlurperClassic().parseText(cDelivery)
 def bStr = jString.btool
 bStr = bStr + " -f appl/pom.xml " + jString.target
-sh "bStr"
+sh "$bStr"
